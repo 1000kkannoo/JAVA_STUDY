56cfb09dd29465d02b03b7430bb582526f7f8655
Hyun Woo <hwsa10041@gmail.com>
2023-04-30 22:00:50 +0900

    [FEAT] 프로그래머스 LV1 문제풀이

---
+++ programmers/src/level1/직사각형별찍기.java
@@ -0,0 +1,18 @@
+package level1;
+
+import java.util.Scanner;
+
+public class 직사각형별찍기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
