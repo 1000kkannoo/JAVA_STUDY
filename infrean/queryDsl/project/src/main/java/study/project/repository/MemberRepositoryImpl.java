6c4ff8858b1f6860b852aebaff33f5efd7aeddd1
Hyun Woo <hwsa10041@gmail.com>
2023-05-25 17:15:30 +0900

    [FEAT] QueryDsl 강의 종료

--- infrean/queryDsl/project/src/main/java/study/project/repository/MemberRepositoryImpl.java
+++ infrean/queryDsl/project/src/main/java/study/project/repository/MemberRepositoryImpl.java
@@ -22,34 +22,17 @@ import static org.springframework.util.StringUtils.hasText;
 import static study.project.entity.QMember.member;
 import static study.project.entity.QTeam.team;
 
-public class MemberRepositoryImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom {
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
-//    public MemberRepositoryImpl(EntityManager em) {
-//        this.queryFactory = new JPAQueryFactory(em);
-//    }
-
     public MemberRepositoryImpl(EntityManager em) {
-        super(Member.class);
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    public List<MemberTeamDto> search(MemberSearchCondition condition) {
 
-        List<MemberTeamDto> result = from(member)
-                .leftJoin(member.team, team)
-                .where(
-                        usernameEq(condition.getUserName()),
-                        teamNameEq(condition.getTeamName()),
-                        ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe()))
-                .select(new QMemberTeamDto(
-                        member.id.as("memberId"),
-                        member.username,
-                        member.age,
-                        team.id.as("teamId"), team.name.as("teamName")))
-                .fetch();
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
 
         return queryFactory
                 .select(new QMemberTeamDto(
@@ -87,31 +70,6 @@ public class MemberRepositoryImpl extends QuerydslRepositorySupport implements M
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetchResults();
-
-        List<MemberTeamDto> content = results.getResults();
-        long total = results.getTotal();
-
-        return new PageImpl<>(content, pageable, total);
-    }
-
-    public Page<MemberTeamDto> searchPageSimple2(MemberSearchCondition condition, Pageable pageable) {
-        JPQLQuery<MemberTeamDto> jpaQuery = from(member)
-                .leftJoin(member.team, team)
-                .where(
-                        usernameEq(condition.getUserName()),
-                        teamNameEq(condition.getTeamName()),
-                        ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe())
-                )
-                .select(new QMemberTeamDto(
-                        member.id.as("memberId"),
-                        member.username,
-                        member.age,
-                        team.id.as("teamId"), team.name.as("teamName")));
-
-        JPQLQuery<MemberTeamDto> query = getQuerydsl().applyPagination(pageable, jpaQuery);
-
-        QueryResults<MemberTeamDto> results = query.fetchResults();
 
         List<MemberTeamDto> content = results.getResults();
         long total = results.getTotal();
---
+++ infrean/queryDsl/project/src/main/java/study/project/repository/MemberTestRepository.java
@@ -0,0 +1,85 @@
+package study.project.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import study.project.dto.MemberSearchCondition;
+import study.project.entity.Member;
+import study.project.entity.QMember;
+import study.project.repository.support.QuerydslRepositoryCustomSupport;
+
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.project.entity.QMember.*;
+import static study.project.entity.QTeam.team;
+
+@Repository
+public class MemberTestRepository extends QuerydslRepositoryCustomSupport {
+
+    public MemberTestRepository() {
+        super(Member.class);
+    }
+
+    public List<Member> basicSelect() {
+        return select(member)
+                .from(member)
+                .fetch();
+    }
+
+    public List<Member> basicSelectFrom() {
+        return selectFrom(member)
+                .fetch();
+    }
+
+    public Page<Member> searchPageApplyPagination(MemberSearchCondition condition, Pageable pageable) {
+        return applyPagination(pageable, query -> query
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUserName()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+        );
+    }
+
+    public Page<Member> searchPageApplyPagination2(MemberSearchCondition condition, Pageable pageable) {
+        return applyPagination(pageable, query -> query
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUserName()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+
+                ), countQuery -> countQuery
+                .select(member.id)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUserName()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+        );
+    }
+
+    private BooleanExpression usernameEq(String userName) {
+        return hasText(userName) ? member.username.eq(userName) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+}
