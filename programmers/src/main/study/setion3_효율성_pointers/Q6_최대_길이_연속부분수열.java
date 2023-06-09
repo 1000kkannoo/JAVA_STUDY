1a3e9874f454466d74efdfdfa7b9fb46b34593c4
Hyun Woo <hwsa10041@gmail.com>
2023-06-09 12:07:18 +0900

    [FEAT] S3Q6_풀이 및 강의 완료

--- programmers/src/main/study/setion3_효율성_pointers/Q6_최대_길이_연속부분수열.java
+++ programmers/src/main/study/setion3_효율성_pointers/Q6_최대_길이_연속부분수열.java
@@ -6,41 +6,24 @@ public class Q6_최대_길이_연속부분수열 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
+        int k = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println(solution(n, m, arr));
+        System.out.println(solution(n, k, arr));
     }
 
-    private static int solution(int n, int m, int[] arr) {
-        int cnt = 0, answer = 0, p1 = 0, p2 = 0;
-        for (int i = 0, j = 0; i < m; j++) {
-            if (arr[j] == 0) {
-                if (p1 == 0) {
-                    p1 = j;
-                } else {
-                    p2 = j;
-                }
-                arr[j] = 1;
-                i++;
+    private static int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+            while (cnt > k) {
+                if (arr[lt] == 0) cnt--;
+                lt++;
             }
+            answer = Math.max(answer, rt - lt + 1);
         }
-
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == 0) {
-                answer = Math.max(answer, cnt);
-                if (p1 != 0 && p2 != 0) {
-                    arr[i] = 1;
-                    arr[p1] = 0;
-                    cnt
-                }
-            } else {
-                cnt++;
-            }
-        }
-
-        return 0;
+        return answer;
     }
 }
