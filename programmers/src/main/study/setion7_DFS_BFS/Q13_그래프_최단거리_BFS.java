d4357760709e25b8427787a75412d4bba044c5cc
Hyun Woo <hwsa10041@gmail.com>
2023-06-13 15:01:03 +0900

    [FEAT] S7Q13_풀이 및 강의 완료

---
+++ programmers/src/main/study/setion7_DFS_BFS/Q13_그래프_최단거리_BFS.java
@@ -0,0 +1,55 @@
+package main.study.setion7_DFS_BFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Q13_그래프_최단거리_BFS {
+    static int n, m;
+    static int[] answer;
+    static ArrayList<ArrayList<Integer>> graph;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        answer = new int[n + 1];
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
+        }
+
+        BFS(1);
+
+        for (int i = 2; i < answer.length; i++) {
+            System.out.println(i + " : " + answer[i]);
+        }
+    }
+
+    private static void BFS(int k) {
+        Queue<ArrayList<Integer>> queue = new LinkedList<>();
+        queue.add(graph.get(k));
+        int level = 1;
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            for (int i = 0; i < length; i++) {
+                ArrayList<Integer> poll = queue.poll();
+                for (int num : poll) {
+                    if (answer[num] == 0) {
+                        answer[num] = level;
+                        queue.add(graph.get(num));
+                    }
+                }
+            }
+            level++;
+        }
+    }
+}
