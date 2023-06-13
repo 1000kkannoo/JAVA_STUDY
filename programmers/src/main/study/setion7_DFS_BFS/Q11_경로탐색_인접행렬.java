a0fd0d9fbc09a1fdbeca3ed41cd766f44aa1aa04
Hyun Woo <hwsa10041@gmail.com>
2023-06-13 13:26:46 +0900

    [FEAT] S7Q11_풀이 및 강의 완료

--- programmers/src/main/study/setion7_DFS_BFS/Q11_경로탐색_인접행렬.java
+++ programmers/src/main/study/setion7_DFS_BFS/Q11_경로탐색_인접행렬.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Q11_경로탐색_인접행렬 {
     static int count = 0;
-    private static int[][] arr;
+    private static int[][] graph;
     private static int[] ch;
 
     public static void main(String[] args) {
@@ -12,26 +12,28 @@ public class Q11_경로탐색_인접행렬 {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        arr = new int[n + 1][n + 1];
+        graph = new int[n + 1][n + 1];
 
         for (int i = 0; i < m; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            arr[a][b] = 1;
+            graph[a][b] = 1;
         }
+        ch = new int[n + 1];
         ch[1] = 1;
         DFS(1);
-        System.out.println();
+        System.out.println(count);
     }
 
-    private static void DFS(int n) {
-        for (int i = n; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                if (arr[i][j] == 1) {
-                    DFS(j);
-                    ch[j] = 1;
+    private static void DFS(int k) {
+        if (k == graph.length - 1) count++;
+        else {
+            for (int i = 1; i < graph.length; i++) {
+                if (graph[k][i] == 1 && ch[i] != 1) {
+                    ch[i] = 1;
+                    DFS(i);
+                    ch[i] = 0;
                 }
-                if (arr[i][])
             }
         }
     }
