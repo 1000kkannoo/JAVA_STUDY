c3fce940b6d14a5e02dd22800b23672e22746ac6
Hyun Woo <hwsa10041@gmail.com>
2023-06-13 19:22:13 +0900

    [FEAT] S8Q2_풀이 및 강의 완료

---
+++ programmers/src/main/study/setion7_DFS_BFS/Q15_바둑이_승차.java
@@ -0,0 +1,30 @@
+package main.study.setion7_DFS_BFS;
+
+import java.util.Scanner;
+
+public class Q15_바둑이_승차 {
+    static int c, max;
+    static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        max = 0;
+        c = sc.nextInt();
+        int n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        DFS(0, 0);
+        System.out.println(max);
+    }
+
+    private static void DFS(int index, int sum) {
+        if (sum > c) return;
+        if (index == arr.length) max = Math.max(max, sum);
+        else {
+            DFS(index + 1, sum + arr[index]);
+            DFS(index + 1, sum);
+        }
+    }
+}
