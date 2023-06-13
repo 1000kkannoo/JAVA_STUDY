22b5c7649ba0943fff2951b5a78eabd2bb45bdc8
Hyun Woo <hwsa10041@gmail.com>
2023-06-13 18:55:40 +0900

    [FEAT] S8Q1_풀이 및 강의 완료

---
+++ programmers/src/main/study/setion7_DFS_BFS/Q14_합이_같은_부분_집합.java
@@ -0,0 +1,33 @@
+package main.study.setion7_DFS_BFS;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q14_합이_같은_부분_집합 {
+    static String answer = "NO";
+    static int[] arr;
+    static int val;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        val = Arrays.stream(arr).sum();
+        System.out.println(DFS(0, 0));
+    }
+
+    private static String DFS(int i, int sum) {
+        if (answer.equals("YES")) return answer;
+        if (sum > val / 2) return answer;
+        if (i == arr.length) {
+            if ((val - sum) == sum) answer = "YES";
+        } else {
+            DFS(i + 1, sum + arr[i]);
+            DFS(i + 1, sum);
+        }
+        return answer;
+    }
+}
