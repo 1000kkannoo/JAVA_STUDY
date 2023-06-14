5ffd454e1c9a61eb4d0924e1fe4ca32f14ea39f4
Hyun Woo <hwsa10041@gmail.com>
2023-06-14 14:11:47 +0900

    [FEAT] S8Q8_풀이 및 강의 완료

--- programmers/src/main/study/setion8_DFS_BFS_활용/Q8_수열_추측하기.java
+++ programmers/src/main/study/setion8_DFS_BFS_활용/Q8_수열_추측하기.java
@@ -3,43 +3,51 @@ package main.study.setion8_DFS_BFS_활용;
 import java.util.Scanner;
 
 public class Q8_수열_추측하기 {
-    static int answerIndex = 0, index = 0;
-    static int[] answer;
-    static int[] sum = new int[2];
     static int[][] dy = new int[35][35];
+    static int[] b, p, ch;
+    static int n, f;
+    static boolean flag = false;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int f = sc.nextInt();
-        answer = new int[n];
-        System.out.println(DFS2(f - 1, 2));
+        // 4
+        n = sc.nextInt();
+        // 16
+        f = sc.nextInt();
+
+        b = new int[n];
+        p = new int[n];
+        ch = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            b[i] = combi(n - 1, i);
+        }
+        DFS(0, 0);
     }
 
-    private static int DFS2(int f, int r) {
-        if (dy[f][r] > 0) return dy[f][r];
-        if (r == 0 || f == r) {
-            int total = 0;
-            for (int num : sum) {
-                total += num;
+    private static void DFS(int index, int sum) {
+        if (index == n) {
+            if (sum == f) {
+                for (int x : p) System.out.print(x + " ");
+                flag = true;
             }
-            return total;
         } else {
-            sum[index] = f;
-            index++;
-            int lt = DFS2(f - 1, r - 1);
-            index--;
-            int rt = DFS2(f - 1, r);
-            dy[f][r] = lt + rt;
-            return dy[f][r] = DFS2(f - 1, r - 1) + DFS2(f - 1, r);
+            if (flag) return;
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    p[index] = i;
+                    DFS(index + 1, sum + (p[index] * b[index]));
+                    ch[i] = 0;
+                }
+            }
         }
     }
 
-    private static void DFS(int n, int r) {
-        if (r == 0 || n == r) answer += 1;
+    private static int combi(int n, int r) {
+        if (dy[n][r] > 0) return dy[n][r];
+        if (r == 0 || n == r) return 1;
         else {
-            DFS(n - 1, r - 1);
-            DFS(n - 1, r);
+            return dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
         }
     }
 }
