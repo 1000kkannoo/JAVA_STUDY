362d0b55b6705fa8987b7797648f197d76d39242
Hyun Woo <hwsa10041@gmail.com>
2023-06-17 16:06:11 +0900

    [FEAT] S10Q1_문제 풀이 및 강의 완료

--- programmers/src/main/study/setion10_동적계획법/S10Q1_계단오르기.java
+++ programmers/src/main/study/setion10_동적계획법/S10Q1_계단오르기.java
@@ -4,19 +4,33 @@ import java.util.Scanner;
 
 public class S10Q1_계단오르기 {
     static int n, answer = 0;
+    static int[] dy;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        DFS(0);
-        System.out.println(answer);
+        dy = new int[n + 1];
+        System.out.println(solution(n));
+//        DFS(0);
+//        System.out.println(answer);
+    }
+
+    private static int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dy[i] = dy[i - 2] + dy[i - 1];
+        }
+        return dy[n];
     }
 
     private static void DFS(int sum) {
         if (sum > n) return;
         if (sum == n) answer++;
         else {
-            for (int i = 1; i <= 2; i++) DFS(sum + i);
+            for (int i = 1; i <= 2; i++) {
+                DFS(sum + i);
+            }
         }
     }
 }
