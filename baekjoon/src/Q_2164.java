23370305ca355ce8677de595674e12ad9fe273ad
Hyun Woo <hwsa10041@gmail.com>
2023-06-20 19:40:22 +0900

    [FEAT] 백준 2164번 풀이

---
+++ baekjoon/src/Q_2164.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Q_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) queue.offer(i);
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
