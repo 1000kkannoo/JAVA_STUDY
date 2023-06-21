99e1f2bf2516eca1d370f4cf9787586723e87aaa
Hyun Woo <hwsa10041@gmail.com>
2023-06-21 12:46:48 +0900

    [FEAT] 백준 1841번 풀이

---
+++ baekjoon/src/Q_1874_H.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Q_1874_H {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+        int start = 0;
+
+        while (N-- > 0) {
+            int value = Integer.parseInt(br.readLine());
+
+            if (value > start) {
+                for (int i = start + 1; i <= value; i++) {
+                    stack.push(i);
+                    sb.append('+').append('\n');
+                }
+                start = value;
+            }
+
+            else if (stack.peek() != value) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append('-').append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
