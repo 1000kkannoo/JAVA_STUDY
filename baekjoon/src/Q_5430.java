9507092a2c5f0fac6ca22bcb1382577168594de5
Hyun Woo <hwsa10041@gmail.com>
2023-06-23 16:27:08 +0900

    [FEAT] 백준 5430번 풀이
    반례 찾기 너무 힘들었따..

--- baekjoon/src/Q_5430.java
+++ baekjoon/src/Q_5430.java
@@ -1,39 +1,62 @@
+import javax.naming.spi.DirObjectFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Q_5430 {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
         for (int i = 0; i < T; i++) {
+            Deque<String> deque = new LinkedList<>();
+
             String functions = br.readLine();
             int n = Integer.parseInt(br.readLine());
-            String list = br.readLine();
 
-            for (char function : functions.toCharArray()) {
-                if (function == 'R') {
-                    char[] chars = list.toCharArray();
-                    int lt = 0;
-                    for (int rt = chars.length - 1; lt != rt; lt++, rt--) {
-                        if (Character.isDigit(chars[lt])) {
-                            char temp = chars[lt];
-                            chars[lt] = chars[rt];
-                            chars[rt] = temp;
-                        }
+            String numList = br.readLine();
+            numList = numList.substring(1, numList.length() - 1);
+
+            st = new StringTokenizer(numList, ",");
+
+            for (int j = 0; j < n; j++) deque.add(st.nextToken());
+
+            boolean isReverse = false;
+            boolean isError = false;
+            for (char ch : functions.toCharArray()) {
+                if (ch == 'R') isReverse = !isReverse;
+                else {
+                    if (deque.isEmpty()) {
+                        isError = true;
+                        break;
                     }
-                    list = String.valueOf(chars);
-                } else {
-                    if (list.length() == 2) {
-                        list = "error";
-                        continue;
-                    }
+                    if (isReverse) deque.pollLast();
+                    else deque.pollFirst();
                 }
             }
-            sb.append(list).append("\n");
+
+            if (isError) sb.append("error").append("\n");
+            else {
+                sb.append("[");
+                if (isReverse) {
+                    while (deque.size() > 1) {
+                        sb.append(deque.pollLast()).append(",");
+                    }
+                } else {
+                    while (deque.size() > 1) {
+                        sb.append(deque.pollFirst()).append(",");
+                    }
+                }
+                if (deque.size() != 0) sb.append(deque.getFirst());
+                sb.append("]").append("\n");
+            }
         }
-        System.out.println(sb);
+
+        System.out.println(sb.deleteCharAt(sb.length() - 1));
     }
 }
