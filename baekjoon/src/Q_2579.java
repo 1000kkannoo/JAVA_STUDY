bcb396702bcfe5a3a76a13e9eb37fa450fadf642
Hyun Woo <hwsa10041@gmail.com>
2023-06-24 14:13:43 +0900

    [FEAT] 백준 2579번 풀이

---
+++ baekjoon/src/Q_2579.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_2579 {
+    static int n;
+    static int[] arr, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n+1];
+        dp = new int[n+1];
+
+        for (int i = 1; i <= n; i++) arr[i] = Integer.parseInt(br.readLine());
+
+        dp[1] = arr[1];
+        if(n >= 2) dp[2] = arr[1] + arr[2];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
