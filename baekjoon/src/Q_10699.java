75a9a0951f7ff98398e092ea50e6ca68804ec1b7
Hyun Woo <hwsa10041@gmail.com>
2023-06-25 10:28:36 +0900

    [FEAT] 백준 10699번 풀이

---
+++ baekjoon/src/Q_10699.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class Q_10699 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now(ZoneId.of("UTC+0")));
+    }
+}
