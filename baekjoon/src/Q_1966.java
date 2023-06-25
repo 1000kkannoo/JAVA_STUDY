61e873efe98ae29a302bd56779b68f5c806f3288
Hyun Woo <hwsa10041@gmail.com>
2023-06-25 15:20:37 +0900

    [FEAT] 백준 1966번 풀이

---
+++ baekjoon/src/Q_1966.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Q_1966 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            LinkedList<int[]> queue = new LinkedList<>();
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < N; i++) {
+                queue.offer(new int[] { i, Integer.parseInt(st.nextToken()) });
+            }
+
+            int count = 0;
+
+            while (!queue.isEmpty()) {
+                int[] front = queue.poll();
+                boolean isMax = true;
+
+                for(int i = 0; i < queue.size(); i++) {
+                    if (front[1] < queue.get(i)[1]) {
+                        queue.offer(front);
+                        for(int j = 0; j < i; j++) {
+                            queue.offer(queue.poll());
+                        }
+
+                        isMax = false;
+                        break;
+                    }
+                }
+
+                if (!isMax) continue;
+
+                count++;
+                if (front[0] == M) {
+                    break;
+                }
+            }
+
+            sb.append(count).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
