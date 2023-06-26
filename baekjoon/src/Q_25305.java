a00d9ddf77fc865e9e4e9bccc844e4e452adbcca
Hyun Woo <hwsa10041@gmail.com>
2023-06-26 18:06:46 +0900

    [FEAT] 백준 25305번 풀이

---
+++ baekjoon/src/Q_25305.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Q_25305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Integer[] arr = new Integer[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, Comparator.reverseOrder());
+        System.out.println(arr[k - 1]);
+    }
+}
