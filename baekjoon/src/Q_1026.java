4686297bb68bb6181c2ca80cb239521d9687a41f
Hyun Woo <hwsa10041@gmail.com>
2023-06-28 15:37:30 +0900

    [FEAT] 백준 1026번 풀이

---
+++ baekjoon/src/Q_1026.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Q_1026 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] A = new int[n];
+        Integer[] B = new Integer[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B, Comparator.reverseOrder());
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += A[i] * B[i];
+        }
+
+        System.out.println(sum);
+    }
+}
