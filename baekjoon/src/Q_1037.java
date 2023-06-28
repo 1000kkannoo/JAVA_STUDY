056c4143dab1561ea483ff21f6885fd9758aa760
Hyun Woo <hwsa10041@gmail.com>
2023-06-28 12:37:16 +0900

    [FEAT] 백준 1037번 풀이

---
+++ baekjoon/src/Q_1037.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q_1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int max = Integer.MIN_VALUE;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] * arr[arr.length - 1]);
+    }
+}
