e1c915904f0d513d6c0a4095b9a370f3e551001d
Hyun Woo <hwsa10041@gmail.com>
2023-06-29 14:55:35 +0900

    [FEAT] 백준 11279번 풀이

---
+++ baekjoon/src/Q_11279.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Q_11279 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(br.readLine());
+            if (a == 0) {
+                int num = (pQ.isEmpty()) ? 0 : pQ.poll();
+                sb.append(num).append("\n");
+            } else {
+                pQ.offer(a);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
