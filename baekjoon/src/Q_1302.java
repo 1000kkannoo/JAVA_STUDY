608cee323cb352d0009342e4959d8c1c95a6d04f
Hyun Woo <hwsa10041@gmail.com>
2023-06-29 18:04:07 +0900

    [FEAT] 백준 1302번 풀이

---
+++ baekjoon/src/Q_1302.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+class Book implements Comparable<Book> {
+    String name;
+    int sales;
+
+    public Book(String name, int sales) {
+        this.name = name;
+        this.sales = sales;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        if (o.sales == this.sales) return this.name.compareTo(o.name);
+        return o.sales - this.sales;
+    }
+}
+
+public class Q_1302 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String book = br.readLine();
+            map.put(book, map.getOrDefault(book, 0) + 1);
+        }
+
+        String maxName = "";
+        ArrayList<Book> arr = new ArrayList<>();
+        for (String name : map.keySet()) {
+            int value = map.get(name);
+            arr.add(new Book(name, value));
+        }
+
+        Collections.sort(arr);
+
+        System.out.println(arr.get(0).name);
+    }
+}
