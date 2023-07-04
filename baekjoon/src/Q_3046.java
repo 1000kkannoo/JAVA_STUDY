ce1d7c71cf426f53edb2dcc45d631b153377347f
Hyun Woo <hwsa10041@gmail.com>
2023-07-04 13:47:11 +0900

    [FEAT] 백준 3064번 풀이

---
+++ baekjoon/src/Q_3046.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_3046 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int r1 = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        System.out.println((s * 2) - r1);
+    }
+}
