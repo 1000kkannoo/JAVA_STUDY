0291e01fbdc46f852328e24f70e0aae312246168
Hyun Woo <hwsa10041@gmail.com>
2023-07-05 13:47:17 +0900

    [FEAT] 백준 28113번 풀이

---
+++ baekjoon/src/Q_28113.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_28113 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a < b) System.out.println("Bus");
+        else if (a > b) System.out.println("Subway");
+        else System.out.println("Anything");
+    }
+}
