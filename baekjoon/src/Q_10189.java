8533b47e2ebeec3b384e93e43196bf217d65fdf3
Hyun Woo <hwsa10041@gmail.com>
2023-07-05 13:50:46 +0900

    [FEAT] 백준 10189번 풀이

---
+++ baekjoon/src/Q_10189.java
@@ -0,0 +1,10 @@
+public class Q_10189 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#  # #### #### #  #").append("\n");
+        sb.append("#### #  # #  # # #").append("\n");
+        sb.append("#### #  # #  # # #").append("\n");
+        sb.append("#  # #### #### #  #");
+        System.out.println(sb);
+    }
+}
