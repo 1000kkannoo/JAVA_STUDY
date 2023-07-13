b0dfe954f6eee3f38babb52b7118900aa67b975d
Hyun Woo <hwsa10041@gmail.com>
2023-07-13 18:31:45 +0900

    [FEAT] 백준 10172번 풀이

---
+++ baekjoon/src/Q_10172.java
@@ -0,0 +1,12 @@
+public class Q_10172 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|").append("\n");
+        sb.append("|q p|   /}").append("\n");
+        sb.append("( 0 )\"\"\"\\").append("\n");
+        sb.append("|\"^\"`    |").append("\n");
+        sb.append("||_/=\\\\__|").append("\n");
+
+        System.out.println(sb);
+    }
+}
