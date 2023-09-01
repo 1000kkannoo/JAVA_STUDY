ef9e1989cb85d92485b68dc7a75f1baded91b281
Hyun Woo <hwsa10041@gmail.com>
2023-09-01 21:01:09 +0900

    Terminal feat : 우아한테크 이전기수 프리코스 1주차 3번 연습

---
+++ practice_wootech/src/main/java/Problem3.java
@@ -0,0 +1,30 @@
+public class Problem3 {
+    public static void main(String[] args) {
+        System.out.println(solution(33));
+    }
+
+    private static int solution(int number) {
+        int[] arr = new int[number + 1];
+
+        for (int i = 1; i <= number; i++) {
+            arr[i] = arr[i - 1] + count369(i);
+        }
+
+        return arr[number];
+    }
+
+    private static int count369(int target) {
+        int count = 0;
+
+        while (target > 0) {
+            int digit = target % 10;
+            if (digit == 3 || digit == 6 || digit == 9) {
+                count++;
+            }
+            target /= 10;
+        }
+
+        return count;
+    }
+
+}
