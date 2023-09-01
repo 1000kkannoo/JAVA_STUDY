4eaa9f028b0e560ddefd820e0c2e1018bc5c87d2
Hyun Woo <hwsa10041@gmail.com>
2023-09-01 20:07:08 +0900

    feat : 우아한테크 이전기수 프리코스 1주차 2번 연습

---
+++ practice_wootech/src/main/java/Problem2.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        System.out.println(solution("browoanoommnaon"));
+    }
+
+    private static String solution(String word) {
+        Stack<Character> stack = removeConsecutiveDuplicates(word);
+        return stackToString(stack);
+    }
+
+    private static String stackToString(Stack<Character> stack) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Character character : stack) {
+            sb.append(character);
+        }
+
+        return sb.toString();
+    }
+
+    private static Stack<Character> removeConsecutiveDuplicates(String word) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            if (!stack.isEmpty() && stack.peek() == word.charAt(i)) {
+                char target = word.charAt(i);
+                while (i < word.length() && word.charAt(i) == target) {
+                    i++;
+                }
+                stack.pop();
+                i--;
+            } else {
+                stack.push(word.charAt(i));
+            }
+        }
+        
+        return stack;
+    }
+}
