047d2b0f2425f51a8b0cc2c129dc0d407b16ea97
Hyun Woo <hwsa10041@gmail.com>
2023-09-06 17:34:10 +0900

    test : Model에서 부족하던 테스트 작성

--- practice_wootech/src/main/java/week2/model/Number.java
+++ practice_wootech/src/main/java/week2/model/Number.java
@@ -6,6 +6,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class Number {
+    public static final int MAX_DIGITS = 3;
+    public static final List<Integer> ALL_DIGITS = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
     private final String num;
 
     public Number(String num) {
@@ -17,12 +20,12 @@ public class Number {
     }
 
     public static Number createRandNumber() {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        List<Integer> numbers = new ArrayList<>(ALL_DIGITS);
         Collections.shuffle(numbers);
 
         StringBuilder computerNumber = new StringBuilder();
-        for (int i = 0; i < 3; i++) {
-            computerNumber.append(numbers.get(i));
+        for (int i = 0; i < MAX_DIGITS; i++) {
+            computerNumber.append(ALL_DIGITS.get(i));
         }
 
         return new Number(computerNumber.toString());
@@ -32,7 +35,7 @@ public class Number {
         String myNum = myNumber.getNum();
         int strike = 0;
 
-        for (int i = 0; i < num.length(); i++) {
+        for (int i = 0; i < MAX_DIGITS; i++) {
             if (num.charAt(i) == myNum.charAt(i)) {
                 strike++;
             }
@@ -46,7 +49,7 @@ public class Number {
         int boll = 0;
 
         String[] split = myNum.split("");
-        for (int i = 0; i < num.length(); i++) {
+        for (int i = 0; i < MAX_DIGITS; i++) {
             if (num.charAt(i) != myNum.charAt(i) && num.contains(split[i])) {
                 boll++;
             }
---
+++ practice_wootech/src/test/java/week2/model/NumberTest.java
@@ -0,0 +1,52 @@
+package week2.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberTest {
+
+    @DisplayName("랜덤으로 각기다른 3자리 숫자를 만들어낸다.")
+    @Test
+    void createRandNumber() {
+        // given
+
+        // when
+        Number randNumber = Number.createRandNumber();
+        String num = randNumber.getNum();
+
+        // then
+        assertThat(num).hasSize(3);  // 3자리 숫자인지 확인
+        assertThat(num.chars().distinct().count()).isEqualTo(3);  // 각 숫자마다 전부 다른 숫자인지 확인
+    }
+
+    @DisplayName("유저가 자신이 입력한 숫자가 몇 strike 인지 확인한다.")
+    @Test
+    void checkStrike() {
+        // given
+        Number computerNumber = new Number("534");
+        Number myNumber = new Number("134");
+
+        // when
+        Integer strike = computerNumber.checkStrike(myNumber);
+
+        // then
+        assertThat(strike).isEqualTo(2);
+    }
+
+    @DisplayName("유저가 자신이 입력한 숫자가 몇 boll 인지 확인한다.")
+    @Test
+    void checkBoll() {
+        // given
+        Number computerNumber = new Number("534");
+        Number myNumber = new Number("523");
+
+        // when
+        Integer boll = computerNumber.checkBoll(myNumber);
+
+        // then
+        assertThat(boll).isEqualTo(1);
+    }
+
+}
