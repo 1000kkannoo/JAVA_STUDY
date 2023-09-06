aa9e2cc86b6a0d4d87a07508aff36e2408f10cbd
Hyun Woo <hwsa10041@gmail.com>
2023-09-06 19:33:29 +0900

    refactor : view 필드 내 상수를 클래스로 분리

--- practice_wootech/src/main/java/week2/view/BaseBallView.java
+++ practice_wootech/src/main/java/week2/view/BaseBallView.java
@@ -7,14 +7,9 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-public class BaseBallView {
+import static week2.view.BaseBallViewConstants.*;
 
-    public static final int ALL_STRIKE = 3;
-    public static final int NO_STRIKE = 0;
-    public static final int NO_BOLL = 0;
-    public static final String GAME_START = "1";
-    public static final String GAME_STOP = "2";
-    public static final Integer NUMBER_LENGTH = 3;
+public class BaseBallView {
 
     private final Scanner scanner;
 
---
+++ practice_wootech/src/main/java/week2/view/BaseBallViewConstants.java
@@ -0,0 +1,12 @@
+package week2.view;
+
+public class BaseBallViewConstants {
+    public static final int ALL_STRIKE = 3;
+    public static final int NO_STRIKE = 0;
+    public static final int NO_BOLL = 0;
+    public static final String GAME_START = "1";
+    public static final String GAME_STOP = "2";
+    public static final Integer NUMBER_LENGTH = 3;
+
+    private BaseBallViewConstants() {}
+}
