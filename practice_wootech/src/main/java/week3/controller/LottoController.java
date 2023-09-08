db025db84ca2be53e5fc4db33f65d9ca3874f09b
Hyun Woo <hwsa10041@gmail.com>
2023-09-08 16:30:33 +0900

    feat : 로또 Controller 구현

---
+++ practice_wootech/src/main/java/week3/controller/LottoController.java
@@ -0,0 +1,37 @@
+package week3.controller;
+
+import week3.model.Lotto;
+import week3.view.LottoView;
+
+import java.util.List;
+import java.util.Map;
+
+public class LottoController {
+    private final LottoView lottoView = new LottoView(System.in);
+
+    public void run() {
+        // 구매금액 입력
+        Integer price = lottoView.inputPurchasePrice();
+        System.out.println();
+
+        // 구매한 개수 및 로또번호 리스트 출력
+        List<Lotto> lottoList = Lotto.createLottoList(price);
+        lottoView.purchaseResult(lottoList);
+        System.out.println();
+
+        // 로또 당첨 번호 입력
+        List<Integer> winningNumbers = lottoView.inputWinningNumbers();
+        System.out.println();
+
+        // 로또 보너스 번호 입력
+        Integer bonusNumber = lottoView.inputBonusNumber();
+        System.out.println();
+
+        // 로또 당첨 확인 및 수익률 계산
+        Map<Integer, Integer> result = Lotto.verifyLottoResult(lottoList, winningNumbers, bonusNumber);
+        String rate = Lotto.calculateProfitRate(result, price);
+
+        // 로또 당첨 통계 출력
+        lottoView.lottoResult(result, rate);
+    }
+}
