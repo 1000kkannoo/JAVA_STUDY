557a94278a60c78e9ca22ca17bc595ad2671ab81
Hyun Woo <hwsa10041@gmail.com>
2023-09-08 16:30:24 +0900

    feat : 로또 Model 구현

---
+++ practice_wootech/src/main/java/week3/model/Lotto.java
@@ -0,0 +1,97 @@
+package week3.model;
+
+import java.text.NumberFormat;
+import java.util.*;
+
+public class Lotto {
+    private static final int LOTTO_MIN = 1;
+    private static final int LOTTO_MAX = 45;
+    private static final int LOTTO_COUNT = 6;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public static String calculateProfitRate(Map<Integer, Integer> result, Integer buy) {
+        int sum = 0;
+
+        // 출력 형식 적용
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMinimumFractionDigits(1);
+        nf.setMaximumFractionDigits(1);
+
+        for (Integer key : result.keySet()) {
+            sum += LottoConstants.calculatePrize(key) * result.get(key);
+        }
+
+        double profitRate = ((double) (sum - buy) / buy) * 100;
+
+        // 소수점 둘째 자리에서 반올림
+        profitRate = Math.round(profitRate * 10) / 10.0;
+
+        return nf.format(100 + profitRate);
+    }
+
+    public static List<Lotto> createLottoList(Integer price) {
+        price /= 1000;
+
+        List<Lotto> LottoList = new ArrayList<>();
+        while (price-- > 0) {
+            LottoList.add(createLotto());
+        }
+
+        return LottoList;
+    }
+
+    public static Lotto createLotto() {
+        List<Integer> settingNumbers = new ArrayList<>();
+        for (int i = LOTTO_MIN; i <= LOTTO_MAX; i++) {
+            settingNumbers.add(i);
+        }
+        Collections.shuffle(settingNumbers);
+
+        List<Integer> randNumbers = settingNumbers.subList(0, LOTTO_COUNT);
+        randNumbers.sort(Comparator.naturalOrder());
+
+        return new Lotto(randNumbers);
+    }
+
+    public static Map<Integer, Integer> verifyLottoResult(
+            List<Lotto> lottoList, List<Integer> winningNumbers, Integer bonusNumber
+    ) {
+        Map<Integer, Integer> resultMap = new HashMap<>();
+
+        for (Lotto lotto : lottoList) {
+            int count = 0;
+            for (Integer number : lotto.getNumbers()) {
+                if (winningNumbers.contains(number)) {
+                    count++;
+                }
+            }
+
+            if (count == 5 && lotto.getNumbers().contains(bonusNumber)) {
+                resultMap.put(0, resultMap.getOrDefault(0, 0) + 1);
+            } else {
+                if (count >= 3) {
+                    System.out.println(count);
+                    resultMap.put(count, resultMap.getOrDefault(count, 0) + 1);
+                }
+            }
+        }
+
+        return resultMap;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
