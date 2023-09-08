e5abed5e0a4d321b30ec4571365e4015cd69515f
Hyun Woo <hwsa10041@gmail.com>
2023-09-08 16:30:41 +0900

    feat : 로또 Main 구현

---
+++ practice_wootech/src/main/java/week3/App.java
@@ -0,0 +1,10 @@
+package week3;
+
+import week3.controller.LottoController;
+
+public class App {
+    public static void main(String[] args) {
+        LottoController lottoController = new LottoController();
+        lottoController.run();
+    }
+}
