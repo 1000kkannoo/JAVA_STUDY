d1d7bc308a49964ec01ed72143660f5590bb92db
Hyun Woo <hwsa10041@gmail.com>
2023-09-10 19:53:17 +0900

    test : 이동할 다리가 U 또는 D 가 아닌 경우 예외처리 테스트 작성

--- practice_wootech/src/test/java/week4/controller/InputViewTest.java
+++ practice_wootech/src/test/java/week4/controller/InputViewTest.java
@@ -50,6 +50,16 @@ class InputViewTest {
         assertThat(bridgeSize).isEqualTo(7);
     }
 
+    @DisplayName("유저가 이동할 다리를 입력 받을때 U 또는 D 가 아닌 입력을 받을 경우 Exception 발생한다.")
+    @Test
+    void validateReadMoving() {
+        // given
+
+        // when // then
+        assertThatThrownBy(() -> inputView.validateReadMoving("L"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이동할 다리는 U 또는 D 만 가능합니다.");
+    }
 
 
 }
