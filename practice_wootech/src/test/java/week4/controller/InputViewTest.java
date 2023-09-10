3233df4cc0a72ffe0c3250c3906397cbde5b499b
Hyun Woo <hwsa10041@gmail.com>
2023-09-10 19:09:16 +0900

    test : 유저가 1 ~ 20 범위 밖의 길이를 입력시 예외처리 테스트 작성

---
+++ practice_wootech/src/test/java/week4/controller/InputViewTest.java
@@ -0,0 +1,25 @@
+package week4.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import week4.view.InputView;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest {
+
+    private final InputView inputView = new InputView();
+
+    @DisplayName("유저가 20 이상의 다리 길이를 입력하면 Exception 발생한다.")
+    @Test
+    void validateReadBridgeSizeOver() {
+        // given
+
+        // when // then
+        assertThatThrownBy(() -> inputView.validateReadBridgeSizeOver(21))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 다리의 길이는 3 이상 20 이하의 길이로 입력하여야 합니다.");
+
+    }
+    
+}
