40efd64bf22afd8bb7ec5bf56d44a05c692c5760
Hyun Woo <hwsa10041@gmail.com>
2023-09-10 20:09:28 +0900

    feat : 게임 재진행/종료 커맨드 입력 기능 구현

--- practice_wootech/src/main/java/week4/view/InputView.java
+++ practice_wootech/src/main/java/week4/view/InputView.java
@@ -50,4 +50,13 @@ public class InputView {
 
         return move;
     }
+
+    public String readGameCommand() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String command = readLine();
+        validateInputIsNull(command);
+        validateReadGameCommand(command);
+
+        return command;
+    }
 }
