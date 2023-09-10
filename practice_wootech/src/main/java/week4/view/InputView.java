2689afcd2e5f165d508017ada6a4cf3e208741b7
Hyun Woo <hwsa10041@gmail.com>
2023-09-10 19:24:34 +0900

    feat : 다리 길이 미 입력시 예외처리 구현

--- practice_wootech/src/main/java/week4/view/InputView.java
+++ practice_wootech/src/main/java/week4/view/InputView.java
@@ -2,10 +2,15 @@ package week4.view;
 
 public class InputView {
 
+    public void validateReadBridgeIsNull(String size) {
+        if (size.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 다리의 길이를 입력하여야 합니다.");
+        }
+    }
+
     public void validateReadBridgeSizeOver(Integer size) {
         if (size < 3 || size > 20) {
             throw new IllegalArgumentException("[ERROR] 다리의 길이는 3 이상 20 이하의 길이로 입력하여야 합니다.");
         }
     }
-
 }
