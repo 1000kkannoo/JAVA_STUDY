ad1a7a2d3e9d48464928f9cc11f34ce9890fba29
Hyun Woo <hwsa10041@gmail.com>
2023-09-10 19:45:52 +0900

    feat : 유저 다리 길이 입력 기능 구현

--- practice_wootech/src/main/java/week4/view/InputView.java
+++ practice_wootech/src/main/java/week4/view/InputView.java
@@ -1,5 +1,7 @@
 package week4.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
 
     public void validateReadBridgeIsNull(String size) {
@@ -12,5 +14,19 @@ public class InputView {
         if (size < 3 || size > 20) {
             throw new IllegalArgumentException("[ERROR] 다리의 길이는 3 이상 20 이하의 길이로 입력하여야 합니다.");
         }
+    }
+
+    public int readBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+        String strBridgeSize = readLine();
+        validateReadBridgeIsNull(strBridgeSize);
+
+        return parseIntBridgeSize(strBridgeSize);
+    }
+
+    private int parseIntBridgeSize(String strBridgeSize) {
+        int bridgeSize = Integer.parseInt(strBridgeSize);
+        validateReadBridgeSizeOver(bridgeSize);
+        return bridgeSize;
     }
 }
