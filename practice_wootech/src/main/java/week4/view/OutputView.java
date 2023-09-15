5fc826094fbd5752eb1d93aa85c904c71310e59c
Hyun Woo <hwsa10041@gmail.com>
2023-09-15 15:04:59 +0900

    feat : OutputView 클래스 생성 및 다리 건너기 게임 시작시 문구 구현

---
+++ practice_wootech/src/main/java/week4/view/OutputView.java
@@ -0,0 +1,15 @@
+package week4.view;
+
+public class OutputView {
+
+    public void printStart() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void printMap() {
+    }
+
+    public void printResult() {
+    }
+
+}
