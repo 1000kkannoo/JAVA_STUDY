45df89d4b867ba4d01ff344d9ca296d7b03c5f8a
Hyun Woo <hwsa10041@gmail.com>
2023-09-17 16:01:34 +0900

    feat : main 구현

--- practice_wootech/src/main/java/week4/Bridge.java
+++ practice_wootech/src/main/java/week4/Bridge.java
@@ -1,7 +1,13 @@
 package week4;
 
-public class Bridge {
-    public static void main(String[] args) {
+import week4.controller.BridgeController;
+import week4.model.BridgeRandomNumberGenerator;
 
+public class Bridge {
+    private static final BridgeController bridgeController = new BridgeController(new BridgeRandomNumberGenerator());
+
+    public static void main(String[] args) {
+        bridgeController.startMessagePrint();
+        bridgeController.playBridgeGame(bridgeController.createBridgeGame());
     }
 }
