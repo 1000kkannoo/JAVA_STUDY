a39e748253c84f4a0ed3c62e623d3e4b7386e7d6
Hyun Woo <hwsa10041@gmail.com>
2023-09-21 20:15:29 +0900

    feat : deleteOrder() 구현

--- practice/src/main/java/practice1/model/OrderManager.java
+++ practice/src/main/java/practice1/model/OrderManager.java
@@ -10,6 +10,18 @@ public class OrderManager {
         this.orders.add(order);
     }
 
+    public void deleteOrder(Long productId) {
+        Order findOrder = findOrderByProductId(productId);
+        orders.remove(findOrder);
+    }
+
+    private Order findOrderByProductId(Long productId) {
+        return orders.stream()
+                .filter(order -> order.getProductId().equals(productId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 Order는 존재하지 않습니다."));
+    }
+
     public List<Order> getOrders() {
         return orders;
     }
