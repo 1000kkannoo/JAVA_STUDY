8643df154c0ecaa18c4ae911083fb1d3cdc8efb9
Hyun Woo <hwsa10041@gmail.com>
2023-09-22 11:33:18 +0900

    feat : printOrderProcess() 구현

---
+++ practice/src/main/java/practice1/view/OutputView.java
@@ -0,0 +1,9 @@
+package practice1.view;
+
+public class OutputView {
+    public void printOrderProcess() {
+        System.out.println("==============================");
+        System.out.println("1.주문 생성 2.주문 수정 3. 주문 삭제 4.주문목록 조회 5.주문 총 금액 조회");
+        System.out.println("==============================");
+    }
+}
