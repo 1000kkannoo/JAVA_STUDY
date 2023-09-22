62cd4b2e889edc1565fc62d948cb05f20d64e345
Hyun Woo <hwsa10041@gmail.com>
2023-09-22 11:56:44 +0900

    feat : 프로세스 종료 추가

--- practice/src/main/java/practice1/view/OutputView.java
+++ practice/src/main/java/practice1/view/OutputView.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class OutputView {
     public void printOrderProcess() {
         System.out.println("==============================");
-        System.out.println("1.주문 생성 2.주문 수정 3. 주문 삭제 4.주문목록 조회 5.주문 총 금액 조회");
+        System.out.println("1.주문 생성 2.주문 수정 3.주문 삭제 4.주문목록 조회 5.주문 총 금액 조회 / 0. 종료");
         System.out.println("==============================");
     }
 
