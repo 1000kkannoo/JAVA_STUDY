80e318407092360554e1b43ab1d8656292bac66e
Hyun Woo <hwsa10041@gmail.com>
2023-10-01 12:33:35 +0900

    feat : 백준 6359번 풀이

---
+++ baekjoon/src/Q_6359.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q_6359 {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T --> 0) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append((int) Math.sqrt(n)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
