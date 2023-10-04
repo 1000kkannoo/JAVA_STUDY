31e064f6dfeac08c8dbd285913d970f6ef789962
Hyun Woo <hwsa10041@gmail.com>
2023-10-04 11:39:13 +0900

    feat : 백준 15000번 풀이

---
+++ baekjoon/src/Q_15000.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q_15000 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().toUpperCase());
+    }
+}
