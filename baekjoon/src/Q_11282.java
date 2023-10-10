692d68b5e335dbd45c8038eba13d28b15c0ac9fd
Hyun Woo <hwsa10041@gmail.com>
2023-10-10 16:25:11 +0900

    feat : 백준 11282번 풀이

---
+++ baekjoon/src/Q_11282.java
@@ -0,0 +1,9 @@
+public class Q_11282 {
+    public static void main(String[] args) {
+        System.out.println(
+                ".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/."
+        );
+    }
+}
