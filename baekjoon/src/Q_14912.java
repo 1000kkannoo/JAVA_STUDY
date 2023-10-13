9e6535e264910c1bcb08cf77c338a5e6fead83da
Hyun Woo <hwsa10041@gmail.com>
2023-10-14 00:05:52 +0900

    feat : 백준 14912번 풀이

---
+++ baekjoon/src/Q_14912.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q_14912 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            String str = String.valueOf(i);
+            for (String s : str.split("")) {
+                if (s.equals(String.valueOf(m))) {
+                    sum++;
+                }
+            }
+        }
+        
+        System.out.println(sum);
+    }
+}
