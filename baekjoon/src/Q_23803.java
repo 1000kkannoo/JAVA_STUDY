cd818995e23c459d36c7c3d82a8ef18e929d6741
Hyun Woo <hwsa10041@gmail.com>
2023-10-17 00:43:14 +0900

    feat : 백준 23803번 풀이

---
+++ baekjoon/src/Q_23803.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q_23803 {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n * 4; i++) {
+            sb.append("@".repeat(n)).append("\n");
+        }
+
+        for (int i = 0; i < n; i++) {
+            sb.append("@".repeat(n * 5)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
