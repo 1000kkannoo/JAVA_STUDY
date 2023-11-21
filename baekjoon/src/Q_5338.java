0333426266d6504767535dda729bd5f63aadc225
Hyun Woo <hwsa10041@gmail.com>
2023-11-21 22:44:59 +0900

    feat : 백준 5338번 풀이

---
+++ baekjoon/src/Q_5338.java
@@ -0,0 +1,10 @@
+public class Q_5338 {
+    public static void main(String[] args) {
+        System.out.println(
+                "       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
