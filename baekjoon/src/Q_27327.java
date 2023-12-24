9a7e257a8053cd41a5a6e1edc87b6f13110c8ad5
Hyun Woo <hwsa10041@gmail.com>
2023-12-25 08:28:56 +0900

    feat : 백준 27327번 풀이

---
+++ baekjoon/src/Q_27327.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q_27327 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int time = Integer.parseInt(br.readLine()) * 24;
+        System.out.println(time);
+    }
+}
