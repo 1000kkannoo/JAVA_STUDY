57659ec4341214ec7fe038bb9e62749b79e56ed4
Hyun Woo <hwsa10041@gmail.com>
2024-01-25 09:11:18 +0900

    feat : 백준 30328번 풀이

---
+++ baekjoon/src/Q_30328.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q_30328 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n * 4000);
+    }
+}
