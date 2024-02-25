c43932fda49b1b5b785a0badfffe814a5adbd83d
1000kkannoo <hwsa10041@gmail.com>
2024-02-25 18:30:03 +0900

    feat : 문자열 섞기 풀이

---
+++ programmers/src/main/level0/문자열_섞기.java
@@ -0,0 +1,16 @@
+package main.level0;
+
+public class 문자열_섞기 {
+    public static void main(String[] args) {
+        System.out.println(solution("aaaaa", "bbbbb"));
+    }
+
+    public static String solution(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str1.length(); i++) {
+            sb.append(str1.charAt(i));
+            sb.append(str2.charAt(i));
+        }
+        return sb.toString();
+    }
+}
