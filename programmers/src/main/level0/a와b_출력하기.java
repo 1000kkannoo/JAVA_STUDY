06476a9a51b5fbe893d500034001075561dc463b
1000kkannoo <hwsa10041@gmail.com>
2024-03-01 09:01:24 +0900

    feat : a와 b 출력하기

---
+++ programmers/src/main/level0/a와b_출력하기.java
@@ -0,0 +1,14 @@
+package main.level0;
+
+import java.util.Scanner;
+
+public class a와b_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("a = %d\n", a);
+        System.out.printf("b = %d", b);
+    }
+}
