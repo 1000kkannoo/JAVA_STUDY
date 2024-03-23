e7c1581aa60ce3eba12807b4156cd3d5052071ab
1000kkannoo <hwsa10041@gmail.com>
2024-03-23 16:27:25 +0900

    feat : 백준 15657번 문제 복습

---
+++ baekjoon/src/Q_15657_복습.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Q_15657_복습 {
+    static int n, m;
+    static int[] print, arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        n = Integer.parseInt(split[0]);
+        m = Integer.parseInt(split[1]);
+        print = new int[m];
+        arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+
+        DFS(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void DFS(int i, int index) {
+        if (i == m) {
+            for (int num : print) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int j = index; j < n; j++) {
+                print[i] = arr[j];
+                DFS(i + 1, j);
+            }
+        }
+    }
+}
