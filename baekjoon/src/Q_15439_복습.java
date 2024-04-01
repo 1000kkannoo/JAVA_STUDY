83f9c089ec9766e5e573d59e167969fc5a011ca2
1000kkannoo <hwsa10041@gmail.com>
2024-04-01 18:02:49 +0900

    feat : 백준 15439번 문제 복습

---
+++ baekjoon/src/Q_15439_복습.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+public class Q_15439_복습 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n * (n - 1));
+    }
+}
