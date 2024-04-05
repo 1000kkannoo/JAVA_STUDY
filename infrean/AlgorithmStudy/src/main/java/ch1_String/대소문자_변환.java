12546944f0d38eae8dd216236296e77a4d8271f1
1000kkannoo <hwsa10041@gmail.com>
2024-04-05 17:28:02 +0900

    feat : 인프런_대소문자 변환 문제 복습

---
+++ infrean/AlgorithmStudy/src/main/java/ch1_String/대소문자_변환.java
@@ -0,0 +1,25 @@
+package ch1_String;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+public class 대소문자_변환 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(solution(str));
+    }
+
+    private static String solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+        return sb.toString();
+    }
+}
