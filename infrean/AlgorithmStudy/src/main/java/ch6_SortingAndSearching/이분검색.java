d7e58fb22986d65f34544283c956458e4e5b3b82
1000kkannoo <hwsa10041@gmail.com>
2024-04-07 19:54:14 +0900

    feat : 인프런_이분검색

---
+++ infrean/AlgorithmStudy/src/main/java/ch6_SortingAndSearching/이분검색.java
@@ -0,0 +1,25 @@
+package ch6_SortingAndSearching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 이분검색 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solution(n, m, arr));
+    }
+
+    private static int solution(int n, int m, int[] arr) {
+        Arrays.sort(arr);
+        return Arrays.binarySearch(arr, m) + 1;
+    }
+}
