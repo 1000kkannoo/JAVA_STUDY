5528a89f44e692f9a3a93f059f66a52bccd3e8e7
1000kkannoo <hwsa10041@gmail.com>
2024-04-10 13:38:57 +0900

    feat : 인프런_소수(에라토스테네스의 체)

---
+++ infrean/AlgorithmStudy/src/main/java/ch2_Array/소수.java
@@ -0,0 +1,27 @@
+package ch2_Array;
+
+import java.util.Scanner;
+
+public class 소수 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solution(n));
+    }
+
+    private static int solution(int n) {
+        int answer = 0;
+        int[] ch = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            if (ch[i] == 0) {
+                answer++;
+                for (int j = i; j <= n; j = j + i) {
+                    ch[j] = 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
